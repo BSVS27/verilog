235ca6f3787785dbc2fd8b671c71d1436584fba3
BSVS <brandonvasu@gmail.com>
2019-08-17 09:56:07 -0600

    RTL completo simulado

--- deco_mid/deco_mid.srcs/sources_1/new/TOP.v
+++ deco_mid/deco_mid.srcs/sources_1/new/TOP.v
@@ -18,7 +18,7 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-module TOP #(SIZE=6) (clk,A,EN,Y);
+module TOP #(SIZE=8) (clk,A,EN,Y); //SIZE es el parámetro que define el número de entradas que se quiere que tenga el deco
 
 localparam mod = SIZE % 2; //Para saber si es par o impar el número de entradas
 
@@ -30,49 +30,26 @@ wire [SIZE-1:0]A0;
 
 FF_top #(.SIZE(SIZE)) inst_FF_top(.clk(clk),.A(A),.Y(A0));
 
-if(SIZE <= 6) begin
+if(SIZE <= 6) begin //Para decodificadores de 6 o menos entradas
+
     wire [63:0]Y0;
-    decoder_top #(.SIZE(SIZE),.mod(mod)) inst_decoder_top(.A(A0[5:0]),.Y(Y0));
-    Enable_2 #(.SIZE(SIZE)) inst_Enable(.A(Y0),.EN(EN),.Y(Y));
-end
-else if(SIZE == 7) begin
-    wire [63:0]Y0;
-    wire [1:0]cable;
-    decoder_top #(.SIZE(6),.mod(0)) inst_decoder_top(.A(A0[5:0]),.Y(Y0));
-    Block_decoder #(.SIZE(1),.SIZE2(0)) inst_block_decoder(.A(A0[6]),.B(),.Y(cable));
-    row_decoder_top #(.SIZE(2)) inst_row_decoder(.EN(EN),.Y_dec(Y0),.Y_blo(cable),.Y(Y));
+    decoder_top #(.SIZE(SIZE),.mod(mod)) inst_decoder_top(.A(A0[5:0]),.Y(Y0)); //Decodificador optimizado
+    Enable_2 #(.SIZE(SIZE)) inst_Enable(.A(Y0),.EN(EN),.Y(Y)); //Etapa de enable
+    
+end else begin //Para decodificadores de más de 6 entradas
+
+    localparam mod1 = (SIZE-6) % 2; //Para saber si es par o impar el número de entradas
+
+    wire [63:0]Y0; //Salidas del deco de 64 
+    wire [(2**(SIZE-6))-1:0] Y1; //Salidas del otro deco
+    
+    decoder_top #(.SIZE(6),.mod(0)) inst_decoder_64(.A(A0[5:0]),.Y(Y0));//Deco 64
+    decoder_top #(.SIZE(SIZE-6),.mod(mod1)) inst_decoder_top(.A(A0[SIZE-1:6]),.Y(Y1));//Otro deco
+    
+    genvar j;
+    for(j=0;j<(2**(SIZE-6));j=j+1) begin
+        row_decoder inst_row_decoder(.EN(EN),.Y_dec(Y0),.Y_blo(Y1[j]),.Y(Y[64*(j+1)-1:64*j])); //Deco de salida con enable
+    end
+    
 end 
-else if(SIZE == 8) begin
-    wire [64*(SIZE-7)-1:0]Y0;
-    wire [4*(SIZE-7)-1:0]cable;
-    decoder_top #(.SIZE(6),.mod(0)) inst_decoder_top(.A(A0[5:0]),.Y(Y0));
-    Block_decoder #(.SIZE(2),.SIZE2(1)) inst_block_decoder(.A(A0[7:6]),.B(),.Y(cable));
-    row_decoder_top #(.SIZE(4)) inst_row_decoder_top(.EN(EN),.Y_dec(Y0),.Y_blo(cable),.Y(Y));
-end
-else if(SIZE==9) begin
-    wire [64*(SIZE-7)-1:0]Y0;
-    wire [4*(SIZE-7)-1:0]cable;
-    wire [2**(SIZE-8)-1:0]Y1;
-    decoder_uno inst_decoder_uno_TOP(.A(A0[8]),.Y(Y1));
-    
-    genvar q;
-    for(q=0;q<(2**(SIZE-8));q=q+1) begin
-        decoder_top #(.SIZE(6),.mod(0)) inst_decoder_top(.A(A0[5:0]),.Y(Y0[64*(q+1)-1:64*q]));
-        Block_decoder #(.SIZE(2),.SIZE2(2)) inst_block_decoder(.A(A0[7:6]),.B(Y1[q]),.Y(cable[4*(q+1)-1:4*q]));
-        row_decoder_top #(.SIZE(4)) inst_row_decoder_top(.EN(EN),.Y_dec(Y0[64*(q+1)-1:64*q]),.Y_blo(cable[4*(q+1)-1:4*q]),.Y(Y[4*64*(q+1)-1:4*64*q]));
-    end
-end else begin
-    wire [64*(2**(SIZE-8))-1:0]Y0;
-    wire [4*(2**(SIZE-8))-1:0]cable;
-    wire [2**(SIZE-8)-1:0]Y1;
-    localparam mod1=(SIZE-8)%2;
-    decoder_top #(.SIZE(SIZE-8),.mod(mod1)) inst_decoder_top_TOP(.A(A0[SIZE-1:8]),.Y(Y1));
-    
-    genvar i;
-    for(i=0;i<(2**(SIZE-8));i=i+1) begin
-        decoder_top #(.SIZE(6),.mod(0)) inst_decoder_top(.A(A0[5:0]),.Y(Y0[64*(i+1)-1:64*i]));
-        Block_decoder #(.SIZE(2),.SIZE2(2)) inst_block_decoder(.A(A0[7:6]),.B(Y1[i]),.Y(cable[4*(i+1)-1:4*i]));
-        row_decoder_top #(.SIZE(4)) inst_row_decoder_top(.EN(EN),.Y_dec(Y0[64*(i+1)-1:64*i]),.Y_blo(cable[4*(i+1)-1:4*i]),.Y(Y[4*64*(i+1)-1:4*64*i]));
-    end
-end
 endmodule
--- deco_mid/deco_mid.srcs/sources_1/new/decoder_top.v
+++ deco_mid/deco_mid.srcs/sources_1/new/decoder_top.v
@@ -18,7 +18,7 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-module decoder_top (A,Y);
+module decoder_top (A,Y);//Decodificador optimizado
 parameter SIZE=0;
 parameter mod=0;
 
@@ -28,72 +28,82 @@ localparam SIZE_wire=(2**SIZE)-4; //Número de cables que ocupo para hacer inter
 input wire [SIZE-1:0] A;
 output wire [SIZE_out-1:0] Y;
 
-wire [SIZE_out+SIZE_wire-1:0] C; //WIRE para hacer las interconecciones
+//wire [SIZE_out+SIZE_wire-1:0] C; //WIRE para hacer las interconecciones
 
-if(mod) begin //IMPAR ==> BASE_NAND;
-    genvar e;
-    for(e=1;e<SIZE;e=e+1) begin
-        if(e==1)begin
-            Base_NAND inst_base_nand(.A(A[1:0]),.Q(C[3:0]));
-        end
-        else begin
-            if(e%2) begin //IMPAR ==> NNANDS / NANDS
-                genvar a;
-                for(a=1;a <= 2**(e-1);a=a+1) begin
-                    if(a <= (2**(e-1))/2)begin //NNANDS
-                        NNANDS inst_nnands(.A(A[e]),.B(C[((2**e)-1)+(a-1)*4:((2**e)-1)+(a-1)*4-3]),.Q(C[((2**(e+1))-1)+(a-1)*4:((2**(e+1))-1)+(a-1)*4-3]));
-                    end
-                    else begin //NANDS
-                        NANDS inst_nands(.A(A[e]),.B(C[((2**e)-1)+((a-(2**(e-1))/2)-1)*4:((2**e)-1)+((a-(2**(e-1))/2)-1)*4-3]),.Q(C[((2**(e+1))-1)+(a-1)*4:((2**(e+1))-1)+(a-1)*4-3]));
+if(SIZE==1) begin
+    wire C;
+    INV inst_inv_block_decoderY0(.A(A),.Q(Y[0]));
+
+    INV inst_inv_block_decoderY1_1(.A(A),.Q(C));
+    INV inst_inv_block_decoderY1_2(.A(C),.Q(Y[1]));
+end else begin
+    wire [SIZE_out+SIZE_wire-1:0] C; //WIRE para hacer las interconecciones
+    //Decodificador optimizado
+    if(mod) begin //IMPAR ==> BASE_NAND;
+        genvar e;
+        for(e=1;e<SIZE;e=e+1) begin
+            if(e==1)begin
+                Base_NAND inst_base_nand(.A(A[1:0]),.Q(C[3:0]));
+            end
+            else begin
+                if(e%2) begin //IMPAR ==> NNANDS / NANDS
+                    genvar a;
+                    for(a=1;a <= 2**(e-1);a=a+1) begin
+                        if(a <= (2**(e-1))/2)begin //NNANDS
+                            NNANDS inst_nnands(.A(A[e]),.B(C[((2**e)-1)+(a-1)*4:((2**e)-1)+(a-1)*4-3]),.Q(C[((2**(e+1))-1)+(a-1)*4:((2**(e+1))-1)+(a-1)*4-3]));
+                        end
+                        else begin //NANDS
+                            NANDS inst_nands(.A(A[e]),.B(C[((2**e)-1)+((a-(2**(e-1))/2)-1)*4:((2**e)-1)+((a-(2**(e-1))/2)-1)*4-3]),.Q(C[((2**(e+1))-1)+(a-1)*4:((2**(e+1))-1)+(a-1)*4-3]));
+                        end
                     end
                 end
-            end
-            else begin //PAR ==> NORS / NNORS
-                genvar b;
-                for(b=1;b <= 2**(e-1);b=b+1) begin
-                    if(b <= (2**(e-1))/2)begin //NORS
-                        NORS inst_nors(.A(A[e]),.B(C[((2**e)-1)+(b-1)*4:((2**e)-1)+(b-1)*4-3]),.Q(C[((2**(e+1))-1)+(b-1)*4:((2**(e+1))-1)+(b-1)*4-3]));
-                    end
-                    else begin //NNORS
-                        NNORS inst_nnors(.A(A[e]),.B(C[((2**e)-1)+((b-(2**(e-1))/2)-1)*4:((2**e)-1)+((b-(2**(e-1))/2)-1)*4-3]),.Q(C[((2**(e+1))-1)+(b-1)*4:((2**(e+1))-1)+(b-1)*4-3]));
-                    end
-                end
-            end
-        end
-    end
-end
-////////////////////////////////////////////////////////////////////////////////////
-else begin //PAR ==> BASE_NOR
-    genvar e;
-    for(e=1;e<SIZE;e=e+1) begin
-        if(e==1)begin
-            Base_NOR inst_base_nor(.A(A[1:0]),.Q(C[3:0]));
-        end
-        else begin
-            if(e%2) begin //IMPAR ==> NORS / NNORS
-                genvar a;
-                for(a=1;a <= 2**(e-1);a=a+1) begin
-                    if(a <= (2**(e-1))/2)begin //NORS
-                        NORS inst_nors(.A(A[e]),.B(C[((2**e)-1)+(a-1)*4:((2**e)-1)+(a-1)*4-3]),.Q(C[((2**(e+1))-1)+(a-1)*4:((2**(e+1))-1)+(a-1)*4-3]));
-                    end
-                    else begin //NNORS
-                        NNORS inst_nnors(.A(A[e]),.B(C[((2**e)-1)+((a-(2**(e-1))/2)-1)*4:((2**e)-1)+((a-(2**(e-1))/2)-1)*4-3]),.Q(C[((2**(e+1))-1)+(a-1)*4:((2**(e+1))-1)+(a-1)*4-3]));
-                    end
-                end
-            end
-            else begin //PAR ==> NNANDS / NANDS
-                genvar b;
-                for(b=1;b <= 2**(e-1);b=b+1) begin
-                    if(b <= (2**(e-1))/2)begin //NNANDS
-                        NNANDS inst_nnands(.A(A[e]),.B(C[((2**e)-1)+(b-1)*4:((2**e)-1)+(b-1)*4-3]),.Q(C[((2**(e+1))-1)+(b-1)*4:((2**(e+1))-1)+(b-1)*4-3]));
-                    end
-                    else begin //NANDS
-                        NANDS inst_nands(.A(A[e]),.B(C[((2**e)-1)+((b-(2**(e-1))/2)-1)*4:((2**e)-1)+((b-(2**(e-1))/2)-1)*4-3]),.Q(C[((2**(e+1))-1)+(b-1)*4:((2**(e+1))-1)+(b-1)*4-3]));
+                else begin //PAR ==> NORS / NNORS
+                    genvar b;
+                    for(b=1;b <= 2**(e-1);b=b+1) begin
+                        if(b <= (2**(e-1))/2)begin //NORS
+                            NORS inst_nors(.A(A[e]),.B(C[((2**e)-1)+(b-1)*4:((2**e)-1)+(b-1)*4-3]),.Q(C[((2**(e+1))-1)+(b-1)*4:((2**(e+1))-1)+(b-1)*4-3]));
+                        end
+                        else begin //NNORS
+                            NNORS inst_nnors(.A(A[e]),.B(C[((2**e)-1)+((b-(2**(e-1))/2)-1)*4:((2**e)-1)+((b-(2**(e-1))/2)-1)*4-3]),.Q(C[((2**(e+1))-1)+(b-1)*4:((2**(e+1))-1)+(b-1)*4-3]));
+                        end
                     end
                 end
             end
         end
     end
+    ////////////////////////////////////////////////////////////////////////////////////
+    else begin //PAR ==> BASE_NOR
+        genvar e;
+        for(e=1;e<SIZE;e=e+1) begin
+            if(e==1)begin
+                Base_NOR inst_base_nor(.A(A[1:0]),.Q(C[3:0]));
+            end
+            else begin
+                if(e%2) begin //IMPAR ==> NORS / NNORS
+                    genvar a;
+                    for(a=1;a <= 2**(e-1);a=a+1) begin
+                        if(a <= (2**(e-1))/2)begin //NORS
+                            NORS inst_nors(.A(A[e]),.B(C[((2**e)-1)+(a-1)*4:((2**e)-1)+(a-1)*4-3]),.Q(C[((2**(e+1))-1)+(a-1)*4:((2**(e+1))-1)+(a-1)*4-3]));
+                        end
+                        else begin //NNORS
+                            NNORS inst_nnors(.A(A[e]),.B(C[((2**e)-1)+((a-(2**(e-1))/2)-1)*4:((2**e)-1)+((a-(2**(e-1))/2)-1)*4-3]),.Q(C[((2**(e+1))-1)+(a-1)*4:((2**(e+1))-1)+(a-1)*4-3]));
+                        end
+                    end
+                end
+                else begin //PAR ==> NNANDS / NANDS
+                    genvar b;
+                    for(b=1;b <= 2**(e-1);b=b+1) begin
+                        if(b <= (2**(e-1))/2)begin //NNANDS
+                            NNANDS inst_nnands(.A(A[e]),.B(C[((2**e)-1)+(b-1)*4:((2**e)-1)+(b-1)*4-3]),.Q(C[((2**(e+1))-1)+(b-1)*4:((2**(e+1))-1)+(b-1)*4-3]));
+                        end
+                        else begin //NANDS
+                            NANDS inst_nands(.A(A[e]),.B(C[((2**e)-1)+((b-(2**(e-1))/2)-1)*4:((2**e)-1)+((b-(2**(e-1))/2)-1)*4-3]),.Q(C[((2**(e+1))-1)+(b-1)*4:((2**(e+1))-1)+(b-1)*4-3]));
+                        end
+                    end
+                end
+            end
+        end
+    end
+    assign Y[SIZE_out-1:0]=C[SIZE_wire+SIZE_out-1:SIZE_wire];
 end
-assign Y[SIZE_out-1:0]=C[SIZE_wire+SIZE_out-1:SIZE_wire];
 endmodule
